69862bc4d6778fb893e845d2fc0ed33677e6e756
Minh Pham <n9623396@qut.edu.au>
2018-10-13 22:46:41 +1000

    Version 1.0.2: Added function: get trending, get stream of tweets using those trends

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var Twitter = require('twitter');
 var router = express.Router();
+var LanguageDetect = require('languagedetect');
+var lngDetector = new LanguageDetect();
+var isAlphanumeric = require('is-alphanumeric');
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+var natural = require('natural');
+var Analyzer = require('natural').SentimentAnalyzer;
+var stemmer = require('natural').PorterStemmer;
+var analyzer = new Analyzer("English", stemmer, "afinn");
+var tokenizer = new natural.WordTokenizer();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -40,12 +50,77 @@ router.post('/', function (req, res, next) {
   		console.log(error);
 	});
 })
+router.post('/hashTags', function (req, res, next) {
+	var client = new Twitter({
+		consumer_key: 'x5SNQCc6zIJHHr5fQqeQobQt1',
+		consumer_secret: 'RTs8lpgKbDpV9StCynNZPssol8wQdtKf9FyTGybKZZnZcQp05F',
+		access_token_key: '1049977195853037569-mqXYZ7ONa1RT3kC2hdfM0geBCBCRq4',
+		access_token_secret: 'aoo4x1sdj0RaBsYDcbBxUyiMotxSXuvzhEdAmmztaxw2J'
+	});
+
+	client.get('trends/place.json', {id : 1, language: "en"}, function (error, trends) {
+		if (error) {
+			console.log(error);
+		}
+		//console.log(trends[0]);
+		let trendHashtags = [];
+		for (let i = 0; i < trends[0].trends.length; i ++) {
+			let trend = trends[0].trends[i].name;
+			//let language  = isAlphanumeric(trend);
+			trendHashtags.push(trend);
+			//console.log(language);
+		}
+		console.log(trendHashtags);
+		console.log(trendHashtags.length);
+		res.json(trendHashtags);
+	})
+
+})
+
+router.post('/stream', function (req, res, next) {
+	var data = req.body.trend;
+	//console.log(data);
+	//var tokenizer = new natural.WordTokenizer();
+	//console.log(tokenizer.tokenize("your dog has fleas."));
+
+	var client = new Twitter({
+		consumer_key: 'x5SNQCc6zIJHHr5fQqeQobQt1',
+		consumer_secret: 'RTs8lpgKbDpV9StCynNZPssol8wQdtKf9FyTGybKZZnZcQp05F',
+		access_token_key: '1049977195853037569-mqXYZ7ONa1RT3kC2hdfM0geBCBCRq4',
+		access_token_secret: 'aoo4x1sdj0RaBsYDcbBxUyiMotxSXuvzhEdAmmztaxw2J'
+	});
+
+
+	var stream = client.stream('statuses/filter', {track: data});
+	stream.on('data', function(data) {
+		let text;
+		if (!data.extended_tweet) {
+			//console.log(data.text);
+			text = data.text;
+		} else {
+			//console.log(data.extended_tweet.full_text);
+			text = data.extended_tweet.full_text;
+		}
+  		//console.log(data.user.name);
+  		let tweet = {
+			Name: data.user.name,
+			timestamp: parseInt(data.timestamp_ms),
+			text: text,
+			source: data.source, 
+			url : data.user.profile_image_url
+		}
+		tweets.push(tweet);
+		//sconsole.log(tweet);
+	});
+ 
+	stream.on('error', function(error) {
+  		console.log(error);
+	});
+})
 
 router.get('/twitter', function (req, res) {
 	let latestTweets = [];
-	let lastSeen = req.query.lastSeen;
-	console.log("lastSeen " + test);
-	console.log("lastSeen (parsed) " + Date.parse(test));
+	//let lastSeen = req.query.lastSeen;
 	if (tweets.length > 1) {
 		for (let i = 0; i < tweets.length; i ++) {
 			let time = tweets[i].timestamp;
@@ -56,6 +131,19 @@ router.get('/twitter', function (req, res) {
 			} else {
 			}
 		}
+
+		function analyseTweet(data) {
+			let results = [];
+			console.log(data.length);
+			for (let i = 0; i < data.length; i++) {
+				let text = data[i].text;
+				let result = sentiment.analyze(text);
+				results.push(result);
+				console.log(result);
+			}
+			return results;
+		}
+		console.log(analyseTweet(latestTweets));
 		res.json(latestTweets);
 	}
 })
